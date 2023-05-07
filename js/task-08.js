f7a875a0dabc9f302729ec74343d8c8f70c3e4c2
Kajetan Antoszkiewicz <wet.kajetan.antoszkiewicz@gmail.com>
2023-05-07 11:59:04 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
-
+'use strict';
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit', handleSubmit);
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+    const formData = {
+        email: email.value,
+        password: password.value,
+    }
+        if (email.value === "" || password.value === "") {
+        alert("Please fill in all the fields!");
+        } else {
+             console.log(formData);   
+    }
+    event.currentTarget.reset();
+};
