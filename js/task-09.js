f142327c653935fba93b8cad5243ae3c3fa82fc6
Kajetan Antoszkiewicz <wet.kajetan.antoszkiewicz@gmail.com>
2023-05-07 12:33:55 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+'use strict';
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+const body = document.querySelector('body');
+const btn = document.querySelector('.change-color');
+const colorName = document.querySelector('.color');
+const changeColor = (event) => {
+  body.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = getRandomHexColor();
+}
+
+btn.addEventListener('click', changeColor );
+
