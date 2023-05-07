a9e3daf3d15881fd91971299ee7f12597975c976
Kajetan Antoszkiewicz <wet.kajetan.antoszkiewicz@gmail.com>
2023-05-07 13:15:21 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,7 @@
 'use strict';
 const sizeControler = document.querySelector('#font-size-control');
 const magicText = document.querySelector('#text');
+sizeControler.value = 16;
 sizeControler.addEventListener('input', () => {
     magicText.style.fontSize = `${sizeControler.value}px`;
 });
